0e8605ff54380782e547d04883ae8881328d3bac
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-11-22 17:02:55 +0100

    style: change display table to html table elements

--- src/components/Historic.tsx
+++ src/components/Historic.tsx
@@ -15,32 +15,37 @@ interface IHistoric {
 
 function Item({ GR_CD, GR_LB, RETAILER_PRODUCT_ID, EAN }: IItem): ReactElement {
   return (
-    <div className={style.row}>
-      <div className={style.cell} title={GR_CD}>{GR_CD}</div>
-      <div className={style.cell} title={GR_LB}>{GR_LB}</div>
-      <div className={style.cell} title={RETAILER_PRODUCT_ID}>{RETAILER_PRODUCT_ID}</div>
-      <div className={style.cell} title={EAN.toString()}>{EAN}</div>
-    </div>
+    <tr className={style.row}>
+      <td className={style.cell} title={GR_CD}>{GR_CD}</td>
+      <td className={style.cell} title={GR_LB}>{GR_LB}</td>
+      <td className={style.cell} title={RETAILER_PRODUCT_ID}>{RETAILER_PRODUCT_ID}</td>
+      <td className={style.cell} title={EAN.toString()}>{EAN}</td>
+    </tr>
   )
 }
 
 export default function Historic({ items }: IHistoric): ReactElement {
   return (
-    <Fragment>
-      <h1>Historique des ajouts</h1>
-      <div id={style.container}>
-       <div className={style.content}>
-        <div className={cls(style.header, style.row)}>
-            <div className={style.cell}>GR_CD</div>
-            <div className={style.cell}>GR_CD</div>
-            <div className={style.cell}>RETAILER_PRODUCT_ID</div>
-            <div className={style.cell}>EAN</div>
-          </div>
-          {items.map((e: IItem, i: number) => (
-            <Item GR_CD={e.GR_CD} GR_LB={e.GR_LB} RETAILER_PRODUCT_ID={e.RETAILER_PRODUCT_ID} EAN={e.EAN} key={i} />
-          ))}
-       </div>
-      </div>
-    </Fragment>
+    <div id={style.container}>
+      <h1 className={style.title}>Historique des ajouts</h1>
+      <table className={style.content}>
+      <tbody>
+        <tr className={cls(style.header, style.row)}>
+          <th className={style.cell}>GR_CD</th>
+          <th className={style.cell}>GR_LB</th>
+          <th className={style.cell}>RETAILER_PRODUCT_ID</th>
+          <th className={style.cell}>EAN</th>
+        </tr>
+        {!items.length && (
+          <tr className={style.row}>
+            <td className={cls(style.cell, style.empty)} colSpan={4}>L'historique est vide.</td>
+          </tr>
+        )}
+        {items.map((e: IItem, i: number) => (
+          <Item GR_CD={e.GR_CD} GR_LB={e.GR_LB} RETAILER_PRODUCT_ID={e.RETAILER_PRODUCT_ID} EAN={e.EAN} key={i} />
+        ))}
+      </tbody>
+      </table>
+    </div>
   );
 }
